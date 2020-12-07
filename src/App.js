4150ccff8bb6f55e633086cadb7f83603ecb3aaf
dwinatech <diwina1993@hotmail.com>
2020-12-07 19:28:10 +0000

    Add commercejs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
   return <div>E-commerce application</div>;
 };
 
