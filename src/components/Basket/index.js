38d8f0a23976d021ac9c05d8d9c05ec208336fdb
dwinatech <diwina1993@hotmail.com>
2020-12-10 14:57:25 +0000

    Create Basket component

---
+++ src/components/Basket/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Grid, Container } from "@material-ui/core";
+import CustomCard from "../CustomCard";
+import "./style.css";
+
+const Basket = ({ basketData, RemoveItemFromBasket }) => {
+  if (!basketData.line_items || !basketData.line_items.length)
+    return <p id="basket">Loading...</p>;
+  return (
+    <Container id="basket">
+      <Grid container justify="center" spacing={4}>
+        {basketData.line_items.map((item) => (
+          <Grid key={item.id} item xs={12} sm={6} md={4}>
+            <CustomCard
+              basket
+              product={item}
+              RemoveItemFromBasket={RemoveItemFromBasket}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Basket;
