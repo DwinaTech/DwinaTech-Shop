249871be2e226c9d31b0d829f4d2b63245026548
dwinatech <diwina1993@hotmail.com>
2020-12-10 14:59:24 +0000

    Refactroing Product and Products components

--- src/App.js
+++ src/App.js
@@ -4,26 +4,53 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { commerce } from "./lib/commerce";
 import NavBar from "./components/NavBar";
 import Products from "./components/Products";
+import Basket from "./components/Basket";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [basketData, setBasketData] = useState({});
+
   const fetchProducts = async () => {
-    const { data } = await commerce.products.list();
-    setProducts(data);
+    const response = await commerce.products.list();
+    setProducts((response && response.data) || []);
+  };
+
+  const fetchBasketData = async () => {
+    const response = await commerce.cart.retrieve();
+    setBasketData(response);
+  };
+
+  const updateBasketData = async (productId) => {
+    const response = await commerce.cart.add(productId, 1);
+    setBasketData(response);
+  };
+
+  const RemoveItemFromBasket = async (itemId) => {
+    const response = await commerce.cart.remove(itemId, 1);
+    setBasketData(response);
   };
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
-  console.log(products);
+    if (!products.length) {
+      fetchProducts();
+    }
+    fetchBasketData();
+  }, [products, basketData]);
+
   return (
     <Router>
       <div>
         <CssBaseline />
-        <NavBar />
+        <NavBar basketItems={basketData.total_items} />
         <Switch>
-          <Route to="/">
-            <Products products={products} />
+          <Route exact path="/">
+            <Products products={products} updateBasketData={updateBasketData} />
+          </Route>
+          <Route exact path="/basket">
+            <Basket
+              basketData={basketData}
+              RemoveItemFromBasket={RemoveItemFromBasket}
+            />
           </Route>
         </Switch>
       </div>
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -4,7 +4,7 @@ import Product from "../Product";
 import Banner from "../Banner";
 import "./style.css";
 
-const Products = ({ products, updateCart }) => {
+const Products = ({ products, updateBasketData }) => {
   if (!products.length) return <p>Loading...</p>;
 
   return (
@@ -14,7 +14,7 @@ const Products = ({ products, updateCart }) => {
         <Grid container justify="center" spacing={4}>
           {products.map((product) => (
             <Grid key={product.id} item xs={12} sm={6} md={4}>
-              <Product product={product} updateCart={updateCart} />
+              <Product product={product} updateBasketData={updateBasketData} />
             </Grid>
           ))}
         </Grid>
