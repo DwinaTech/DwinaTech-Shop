498944c35ddf25f5ed1516556a9e5f756d540e39
dwinatech <diwina1993@hotmail.com>
2020-12-28 03:35:20 +0000

    Remove node_modules

--- client/node_modules/@stripe/react-stripe-js/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Changelog
-
-See the [releases page](https://github.com/stripe/react-stripe-js/releases) on
-GitHub for release notes.
--- client/node_modules/@stripe/react-stripe-js/src/env.d.ts
+++
@@ -1,4 +0,0 @@
-// A magic global reflecting the current package version defined in
-// `package.json`. This will be rewritten at build time as a string literal
-// when rollup is run (via `@plugin/rollup-replace`).
-declare const _VERSION: string;
