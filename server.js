dc779f867f5bbd63724c60b6de2f16f5bb154ffc
dwinatech <diwina1993@hotmail.com>
2020-12-28 03:46:43 +0000

    Create gitigonre file and correct client path on  server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
  */
 //
 if (process.env.NODE_ENV !== "development") {
-  app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+  app.use("/", express.static(path.resolve(__dirname, "./client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 
