8c229bf6d23d154722f62caf273f065df403baae
dwinatech <diwina1993@hotmail.com>
2021-02-02 15:49:51 +0000

    Create Spinner component

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,10 @@
+import { CircularProgress } from "@material-ui/core";
+import "./style.css";
+
+const Spinner = () => (
+  <div className="spinner">
+    <CircularProgress />
+  </div>
+);
+
+export default Spinner;
