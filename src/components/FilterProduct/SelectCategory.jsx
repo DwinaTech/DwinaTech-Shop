08eb8ed2c828d72df6a9ae69924c0e6ee72e9209
Mohammed Dwina <diwina1993@hotmail.com>
2021-06-30 23:36:38 +0100

    Improve the UI and display search results

--- src/components/FilterProduct/SelectCategory.jsx
+++ src/components/FilterProduct/SelectCategory.jsx
@@ -1,19 +1,18 @@
 import { FormControl, Select, MenuItem } from "@material-ui/core";
-import './style.css'
+import "./style.css";
 
-const SelectCategory = () => {
-    const age = 0
-    const handleChange = () => { }
-    return <FormControl className="formControl">
-        <Select
-            value={age}
-            onChange={handleChange}
-        >
-            <MenuItem value={0}>All</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
+const SelectCategory = ({ onChange, selectedCategory, categories }) => {
+  return (
+    <FormControl className="formControl">
+      <Select value={selectedCategory.id} onChange={onChange}>
+        {categories.map((category) => (
+          <MenuItem key={category.id} value={category.id}>
+            {category.name}
+          </MenuItem>
+        ))}
+      </Select>
     </FormControl>
-}
+  );
+};
 
 export default SelectCategory;
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Grid, Container, Typography } from "@material-ui/core";
 import Product from "../Product";
 import FilterProduct from "../FilterProduct";
@@ -6,45 +7,59 @@ import Banner from "../Banner";
 import "./style.css";
 
 const Products = ({ categories, addProduct }) => {
+  const [searchResult, setSearchResult] = React.useState([]);
   if (!categories.length) return <Spinner />;
 
   return (
     <div>
-      <FilterProduct />
-      <Banner />
-      <div id="products">
-        {categories.map((category, index) =>
-          category.productsData.length ? (
-            <div
-              key={category.id}
-              className="contents"
-              style={{
-                backgroundImage:
-                  index % 2 !== 0
-                    ? "linear-gradient(to bottom right, #3d4a5d,#3d4a5d, #bb86fc)"
-                    : "",
-              }}
-            >
-              <Container>
-                <Typography className="headline" variant="h3" component="h2">
-                  {category.name}
-                </Typography>
-                <Grid container spacing={4}>
-                  {category.productsData.map((product) => (
-                    <Grid key={product.id} item xs={12} sm={6} md={4}>
-                      <Product
-                        product={product}
-                        addProduct={addProduct}
-                        categoryName={category.name}
-                      />
+      <FilterProduct
+        addProduct={addProduct}
+        categories={categories}
+        searchResult={searchResult}
+        setSearchResult={setSearchResult}
+      />
+      {!searchResult.length && (
+        <>
+          <Banner />
+          <div id="products">
+            {categories.map((category, index) =>
+              category.productsData.length ? (
+                <div
+                  key={category.id}
+                  className="contents"
+                  style={{
+                    backgroundImage:
+                      index % 2 !== 0
+                        ? "linear-gradient(to bottom right, #3d4a5d,#3d4a5d, #bb86fc)"
+                        : "",
+                  }}
+                >
+                  <Container>
+                    <Typography
+                      className="headline"
+                      variant="h3"
+                      component="h2"
+                    >
+                      {category.name}
+                    </Typography>
+                    <Grid container spacing={4}>
+                      {category.productsData.map((product) => (
+                        <Grid key={product.id} item xs={12} sm={6} md={4}>
+                          <Product
+                            product={product}
+                            addProduct={addProduct}
+                            categoryName={category.name}
+                          />
+                        </Grid>
+                      ))}
                     </Grid>
-                  ))}
-                </Grid>
-              </Container>
-            </div>
-          ) : null
-        )}
-      </div>
+                  </Container>
+                </div>
+              ) : null
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
